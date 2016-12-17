e9d97beef9b2bcecd7a6a80ac686c65ed5884b6d
luyao chong <luyao chong>
2016-12-17 21:18:38 +0800

    left filter -front-end

--- static/styles/js/select.js
+++ static/styles/js/select.js
@@ -1,71 +1,86 @@
+// filter:{"state","city","time","category","score","service","environment","taste"}
+var filter = new Object(); 
+
+function init()
+{
+	filter.state = "null";
+	filter.city = "null";
+	filter.time = "null";
+	filter.category = "null";
+	filter.score = "null";
+	filter.service = "null";
+	filter.environment = "null";
+	filter.taste = "null";
+}
+
 //生成所有选择框
 function generate_all_query(){
 	// select-state
-	var select_state = $("#select-state select");
-	add_option(select_state, "不限");
-	add_option(select_state, "Alabama");
-	add_option(select_state, "Alaska");
-	add_option(select_state, "Arizona");
-	add_option(select_state, "Arkansas");
-	add_option(select_state, "California");
-	add_option(select_state, "Colorado");
-	add_option(select_state, "Connecticut");
-	add_option(select_state, "Delaware");
-	add_option(select_state, "Florida");
-	add_option(select_state, "Georgia");
-	add_option(select_state, "Hawaii");
-	add_option(select_state, "Idaho");
-	add_option(select_state, "Illinois");
-	add_option(select_state, "Indiana");
-	add_option(select_state, "Iowa");
-	add_option(select_state, "Kansas");
-	add_option(select_state, "Kentucky");
-	add_option(select_state, "Louisiana");
-	add_option(select_state, "Maine");
-	add_option(select_state, "Maryland");
-	add_option(select_state, "Massachusetts");
-	add_option(select_state, "Michigan");
-	add_option(select_state, "Minnesota");
-	add_option(select_state, "Mississippi");
-	add_option(select_state, "Missouri");
-	add_option(select_state, "Montana");
-	add_option(select_state, "Nebraska");
-	add_option(select_state, "Nevada");
-	add_option(select_state, "New Hampshire");
-	add_option(select_state, "New Jersey");
-	add_option(select_state, "New Mexico");
-	add_option(select_state, "New York");
-	add_option(select_state, "North Carolina");
-	add_option(select_state, " North Dakota");
-	add_option(select_state, "Ohio");
-	add_option(select_state, "Oklahoma");
-	add_option(select_state, "Oregon");
-	add_option(select_state, "Pennsylvania");
-	add_option(select_state, "Rhode Island");
-	add_option(select_state, "South Carolina");
-	add_option(select_state, "South Dakota");
-	add_option(select_state, "Tennessee");
-	add_option(select_state, "Texas");
-	add_option(select_state, "Utah");
-	add_option(select_state, "Vermont");
-	add_option(select_state, "Virginia");
-	add_option(select_state, "Washington");
-	add_option(select_state, "West Virginia");
-	add_option(select_state, "Wisconsin");
-	add_option(select_state, "Wyoming");
+	var filter_state = $("#filter-state select");
+	add_option(filter_state, "不限");
+	add_option(filter_state, "Alabama");
+	add_option(filter_state, "Alaska");
+	add_option(filter_state, "Arizona");
+	add_option(filter_state, "Arkansas");
+	add_option(filter_state, "California");
+	add_option(filter_state, "Colorado");
+	add_option(filter_state, "Connecticut");
+	add_option(filter_state, "Delaware");
+	add_option(filter_state, "Florida");
+	add_option(filter_state, "Georgia");
+	add_option(filter_state, "Hawaii");
+	add_option(filter_state, "Idaho");
+	add_option(filter_state, "Illinois");
+	add_option(filter_state, "Indiana");
+	add_option(filter_state, "Iowa");
+	add_option(filter_state, "Kansas");
+	add_option(filter_state, "Kentucky");
+	add_option(filter_state, "Louisiana");
+	add_option(filter_state, "Maine");
+	add_option(filter_state, "Maryland");
+	add_option(filter_state, "Massachusetts");
+	add_option(filter_state, "Michigan");
+	add_option(filter_state, "Minnesota");
+	add_option(filter_state, "Mississippi");
+	add_option(filter_state, "Missouri");
+	add_option(filter_state, "Montana");
+	add_option(filter_state, "Nebraska");
+	add_option(filter_state, "Nevada");
+	add_option(filter_state, "New Hampshire");
+	add_option(filter_state, "New Jersey");
+	add_option(filter_state, "New Mexico");
+	add_option(filter_state, "New York");
+	add_option(filter_state, "North Carolina");
+	add_option(filter_state, " North Dakota");
+	add_option(filter_state, "Ohio");
+	add_option(filter_state, "Oklahoma");
+	add_option(filter_state, "Oregon");
+	add_option(filter_state, "Pennsylvania");
+	add_option(filter_state, "Rhode Island");
+	add_option(filter_state, "South Carolina");
+	add_option(filter_state, "South Dakota");
+	add_option(filter_state, "Tennessee");
+	add_option(filter_state, "Texas");
+	add_option(filter_state, "Utah");
+	add_option(filter_state, "Vermont");
+	add_option(filter_state, "Virginia");
+	add_option(filter_state, "Washington");
+	add_option(filter_state, "West Virginia");
+	add_option(filter_state, "Wisconsin");
+	add_option(filter_state, "Wyoming");
 	
 	//select-time
-	var select_time = $("#select-time select");
-	add_option(select_time, "不限");
-	add_option(select_time, "Weekday");
-	add_option(select_time, "Weekend");
-	add_option(select_time, "Monday");	
-	add_option(select_time, "Tuesday");	
-	add_option(select_time, "Wednesday");	
-	add_option(select_time, "Thursday");	
-	add_option(select_time, "Friday");	
-	add_option(select_time, "Saturday");	
-	add_option(select_time, "Sunday");
+	var filter_time = $("#filter-time select");
+	add_option(filter_time, "不限");
+	add_option(filter_time, "Weekday");
+	add_option(filter_time, "Weekend");
+	add_option(filter_time, "Monday");	
+	add_option(filter_time, "Tuesday");	
+	add_option(filter_time, "Wednesday");	
+	add_option(filter_time, "Thursday");	
+	add_option(filter_time, "Friday");	
+	add_option(filter_time, "Saturday");	
+	add_option(filter_time, "Sunday");
 }
 function add_option(dst, content){
 	var select = $(dst);
@@ -75,8 +90,38 @@ function add_option(dst, content){
 }
 
 //select选择响应
-function get_city_list(){
-	var state = $("#select-state select").val();
+function send_filter_query()
+{
+	// var str_filter = '{"filter":' + '{"state":"' + filter.state + '"},"' + '{"city":' + filter.city + '"},' + '{"time":' + filter.time + '"},' + '{"category":' + filter.category + '"},' + '{"score":' + filter.score + '"},' + '{"service":' + filter.service + '"},' + '{"environment":' + filter.environment + '"},' + '{"taste":' + filter.taste + '"}' + '}';
+	var xmlhttp;
+	if (window.XMLHttpRequest)
+	{
+		xmlhttp = new XMLHttpRequest();
+	}
+	else
+	{
+		xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+	}
+	var str_filter = 'state=' + filter.state + '&' + 'city=' + filter.city + '&' + 'time=' + filter.time + '&' + 'category=' + filter.category;
+	xmlhttp.open("GET",("/query/filter_result.json?"+str_filter),true);
+	xmlhttp.send();
+	xmlhttp.onreadystatechange=function()
+	{
+	    if (xmlhttp.readyState==4 && xmlhttp.status==200)
+	    {
+	        var data = JSON.parse(xmlhttp.responseText);
+	        var select_city = $("#filter-city select");
+	        for (var i = 0;i < data.cities.length;i ++)
+	        {
+	        	add_option(select_city, data.cities[i]);
+	        }
+	    }
+	}
+}
+function filter_state_changed(){
+	var state = $("#filter-state select").val();
+	console.log(state);
+	filter.state = state;
 	if (state != "不限")
 	{
 		var xmlhttp;
@@ -95,25 +140,79 @@ function get_city_list(){
 		    if (xmlhttp.readyState==4 && xmlhttp.status==200)
 		    {
 		        var data = JSON.parse(xmlhttp.responseText);
-		        var select_city = $("#select-city select");
+		        var select_city = $("#filter-city select");
 		        for (var i = 0;i < data.cities.length;i ++)
 		        {
 		        	add_option(select_city, data.cities[i]);
 		        }
 		    }
 		}
-		$("#select-city").css("display", "block");
+		$("#filter-city").css("display", "block");
 	}
 	else
 	{
-		$("#select-city").css("display", "none");
+		$("#filter-city").css("display", "none");
 	}
 }
-function select_other_query(){
+function filter_query_changed(id){
+	switch (id)
+	{
+		case "filter-city":
+		{
+			filter.city = $("#filter-city select").val();
+			console.log("city");
+			break;
+		}
+		case "filter-time":
+		{
+			filter.time = $("#filter-time select").val();
+			break;
+		}
+		case "filter-category":
+		{
+			filter.category = $("#filter-category select").val();
+			break;
+		}
+		case "filter-score":
+		{
+			filter.score = $("#filter-score select").val();
+			break;
+		}
+		case "filter-service":
+		{
+			filter.service = $("#filter-service select").val();
+			break;
+		}
+		case "filter-environment":
+		{
+			filter.environment = $("#filter-environment select").val();
+			break;
+		}
+		case "filter-taste":
+		{
+			filter.taste = $("#filter-taste select").val();
+			break;
+		}
+		default:
+		{
 
+		}
+	}
+	send_filter_query();
+}
+function filter_changed(){
+	if (this.id == "filter-state")
+	{
+		filter_state_changed();
+	}
+	else
+	{
+		filter_query_changed(this.id);
+	}
 }
 
 
 // to do
+init();
 generate_all_query();
-$('#select-state select').change(get_city_list);
+$('#filter1 [id]').change(filter_changed);
